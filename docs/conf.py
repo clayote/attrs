8e006fc0b4f44d331973c5c43f2a8b6d1d3ff880
Zachary Spector <public@zacharyspector.com>
2026-02-22 07:00:04 +1300

    Make the Sphinx extension public

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "_ext"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "ext"))
 
 from importlib import metadata
 from pathlib import Path
